ca1c80869eae52a149c73d8e1c91029f342871dd
William Dickinson <wwd35@email.unc.edu>
2022-01-22 22:49:51 -0500

    Updated Chardle Assignment

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -20,19 +20,19 @@ print("Searching for " + letter + " in " + word)
 
 if letter is word[0]:
     print(letter + " found at index 0.")
-    instance: int = instance + 1
+    instance = instance + 1
 if letter is word[1]:
     print(letter + " found at index 1.")
-    instance: int = instance + 1
+    instance = instance + 1
 if letter is word[2]:
     print(letter + " found at index 2.")
-    instance: int = instance + 1
+    instance = instance + 1
 if letter is word[3]:
     print(letter + " found at index 3.")
-    instance: int = instance + 1
+    instance = instance + 1
 if letter is word[4]:
     print(letter + " found at index 4.")
-    instance: int = instance + 1
+    instance = instance + 1
 
 if instance == 1:
     print(str(instance) + " instance of " + letter + " found in " + word)
